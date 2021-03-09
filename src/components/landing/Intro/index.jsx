031076ab58a8654ecb26a90ad8c1bff09f0a81cc
Jiří Kusák <jirik@kentico.com>
2021-03-09 21:36:49 +0100

    Starter

---
+++ src/components/landing/Intro/index.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { ThemeContext } from 'providers/ThemeProvider';
+import { Header } from 'components/theme';
+import { Container, Button } from 'components/common';
+import dev from 'assets/illustrations/dev.svg';
+import { Wrapper, IntroWrapper, Details, Thumbnail } from './styles';
+
+export const Intro = () => {
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <Wrapper>
+      <Header />
+      <IntroWrapper as={Container}>
+        <Details theme={theme}>
+          <h1>Vítejte u nás!</h1>
+          <p>Doufáme, že tyto stránky ještě prohloubí a zkvalitní vzájemnou komunikaci mezi námi a Vámi, našimi pacienty.</p>
+          <Button as={AnchorLink} href="#contact">
+            Hire me
+          </Button>
+        </Details>
+        <Thumbnail>
+          <img src={dev} alt="I’m John and I’m a JAMStack engineer!" />
+        </Thumbnail>
+      </IntroWrapper>
+    </Wrapper>
+  );
+};
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Layout, SEO } from 'components/common';
+import { Intro, Skills, Contact, Projects } from 'components/landing';
+
+export default () => (
+  <Layout>
+    <SEO />
+    <Intro />
+    <Skills />
+    <Contact />
+  </Layout>
+);
