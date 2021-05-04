083761363b999f28852b856b47bbe133f978c342
Jiří Kusák <jirik@kentico.com>
2021-05-04 14:21:49 +0200

    Update time block component

--- src/components/common/TimeBlock/index.js
+++ src/components/common/TimeBlock/index.js
@@ -4,14 +4,30 @@ export const TimeBlock = ({ name, blocks }) => {
   return (
     <>
       <h2>{name}</h2>
-      {blocks.map((blok) => (
-        <p>
-          {blok.elements.zacatek.value} - {blok.elements.konec.value}
-          {blok.elements.poznamka.value && (
-            <span> ({blok.elements.poznamka.value})</span>
-          )}
-        </p>
-      ))}
+      {blocks.map((blok) => {
+        const isHoursWithNote =
+          blok.elements.zacatek.value && blok.elements.poznamka.value;
+        const isHoursOnly =
+          blok.elements.zacatek.value && !!!blok.elements.poznamka.value;
+        const isNoteOnly =
+          !!!blok.elements.zacatek.value && blok.elements.poznamka.value;
+        return (
+          <p>
+            {isHoursWithNote && (
+              <span>
+                {blok.elements.zacatek.value} - {blok.elements.konec.value} (
+                {blok.elements.poznamka.value})
+              </span>
+            )}
+            {isHoursOnly && (
+              <span>
+                {blok.elements.zacatek.value} - {blok.elements.konec.value}
+              </span>
+            )}
+            {isNoteOnly && <span>{blok.elements.poznamka.value}</span>}
+          </p>
+        );
+      })}
     </>
   );
 };
